fa3884759288dd83ecd500d73ca180cfa3af9541
Ryota Suzuki <ryotasuzuki@RyotanoMacBook-puro.local>
2018-08-07 19:22:10 +0900

    postしたデータを作成して保存する。

--- app.rb
+++ app.rb
@@ -8,5 +8,14 @@ get '/' do
 end
 
 post '/' do
+  client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'booklist', encoding: 'utf8')
+
+  #書籍タイトルの保存
+  book_title = params['book_title']
+  statement = client.prepare('INSERT INTO books (book_title) VALUES(?)')
+  statement.execute(book_title)
+
+  @records = client.query("SELECT * FROM books ORDER BY created_at DESC")
+
   erb :booklist
 end
